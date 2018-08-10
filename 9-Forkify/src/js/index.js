197912179cbc2d73b4b0abe7cfbad65fd5a1cd3e
Brian Truong <bstruong1@gmail.com>
2018-08-10 13:55:09 -0700

    Forkify: Babel setup

--- 9-Forkify/src/js/index.js
+++ 9-Forkify/src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import num from './test';
+const x = 69;
 
-console.log(`Imported ${num} from another module`);
+console.log(`Imported ${num} from another module, ${x} is a const`);
